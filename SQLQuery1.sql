6c499c7cc32e4e1c1fe667a5433ee61e2791ad3e
Marijn <42944527+ThatGuyMarijn@users.noreply.github.com>
2020-02-14 14:24:01 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿SELECT *
+FROM Films film
+INNER JOIN FilmVertoningen filmVert
+	on film.Id = filmVert.FilmId
+INNER JOIN ReserveringenVertoningen resVert
+	on filmVert.Id = resVert.VertoningsId
+INNER JOIN Reserveringen res
+	on resVert.ReserveringsId = res.ReserveringsId
+INNER JOIN AspNetUsers users
+	on res.KlantId = users.Id
+WHERE res.KlantId like '%bfe1524b-2be9-4337-8832-96ddc56c2ea7%'
---
+++ Startup.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Cinema7.Areas.Identity;
+using Cinema7.Data;
+using Cinema7.Data.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Cinema7
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CinemaDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+
+            // Delete this in future? (MijnBestellingen.razor.cs)
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddScoped<FilmController>();
+            services.AddScoped<VertoningController>();
+            services.AddScoped<UserController>();
+            services.AddScoped<ReserveringController>();
+            services.AddScoped<BestellingController>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
